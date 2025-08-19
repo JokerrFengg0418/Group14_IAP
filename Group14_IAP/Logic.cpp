3afcd0566e25309ab2bfb87acecd331572510e55
Themiei <themieigab@gmail.com>
2025-08-19 15:02:17 +0800

    Added Basic Logic First. Specifics to be done after Combat is FInalized

--- Group14_IAP/Logic.cpp
+++ Group14_IAP/Logic.cpp
@@ -1,1 +1,73 @@
 #include "Logic.h"
+
+Logic::Logic() {
+
+	GameOrderState = 1;
+	GlobalOrderState = 1;
+	GameEndState = false;
+
+}
+
+
+
+
+
+void Logic::GameOrderStateSet(int CurrentGameOrder) {
+	
+	GameOrderState = CurrentGameOrder;
+
+}
+
+
+
+
+int Logic::GameOrderStateGet() {
+
+	return GameOrderState;
+
+}
+
+
+
+
+void Logic::GameEndStateIncrease() {
+
+	GameEndState += 1;
+
+}
+
+
+
+
+bool Logic::GameEndStateGet() {
+
+	return GameEndStateGet;
+
+}
+
+
+
+
+int Logic::GlobalOrderGet() const{
+
+	return GlobalOrderState;
+
+}
+
+
+
+
+void Logic::GlobalOrderSet(int CurrentGlobalOrder) {
+
+	GlobalOrderState = CurrentGlobalOrder;
+
+}
+
+
+
+
+void Logic::TurnOrder() {
+
+
+
+}
--- Group14_IAP/Logic.h
+++ Group14_IAP/Logic.h
@@ -1,5 +1,40 @@
 #pragma once
 class Logic
 {
+private:
+	// Determines what part of the game to access//
+	int GameOrderState;
+
+	//Determines what progression the Player is at//
+	int GlobalOrderState;
+
+	//End Condition of the Game//
+	bool GameEndState;
+public:
+
+	//Constructor//
+	Logic();
+
+	//Game Order Setter//
+	void GameOrderStateSet(int CurrentGameOrder);
+
+	//Game Order Getter//
+	int GameOrderStateGet();
+
+	// Game End State increases//
+	void GameEndStateIncrease();
+
+	//Game End State Get//
+	bool GameEndStateGet();
+
+	//Global Order Get//
+	int GlobalOrderGet() const;
+
+	//Global Order Set//
+	void GlobalOrderSet(int CurrentGlobalOrder);
+
+	//Turn Order//
+	void TurnOrder();
+
 };
 
