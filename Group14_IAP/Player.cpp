9c3e08b0431c0a236f69a59b5d306486dc89843d
Jovan Lim <jovanlat9418@gmail.com>
2025-08-19 15:39:49 +0800

    Player n shi

--- Group14_IAP/Player.cpp
+++ Group14_IAP/Player.cpp
@@ -1,9 +1,12 @@
 #include "Player.h"
 #include <conio.h>
 #include <iostream>
+#include "Position.h"
 
 void Player::move()
 {
+	int newX = position.row;
+	int newY = position.col;
 
 	std::cout << "Use WASD to move\n";
 
@@ -13,18 +16,22 @@ void Player::move()
 	switch (input) {
 	case 'w':
 	case 'W':
+		newY++;
 		std::cout << "Move Up \n";
 		break;
 	case 's':
 	case 'S':
+		newY--;
 		std::cout << "Move Down \n";
 		break;
 	case 'a':
 	case 'A':
+		newX--;
 		std::cout << "Move Left \n";
 		break;
 	case 'd':
 	case 'D':
+		newX++;
 		std::cout << "Move Right \n";
 		break;
 	default:
--- Group14_IAP/Position.h
+++ Group14_IAP/Position.h
@@ -1,11 +1,10 @@
 #pragma once
 class Position {
-private:
-    int row, col;
 public:
     Position(int r = 0, int c = 0);
     int getRow() const;
     int getCol() const;
     void setPosition(int r, int c);
+    int row, col;
 };
 
