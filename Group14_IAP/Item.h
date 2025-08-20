15c06d4a81235bffa193e0f055e98a3ab45e4152
Themiei <themieigab@gmail.com>
2025-08-20 09:11:17 +0800

    Item Class made

--- Group14_IAP/Item.h
+++ Group14_IAP/Item.h
@@ -1,5 +1,36 @@
 #pragma once
+#include <string>
 class Item
 {
+private:
+
+	std::string ItemName;
+	std::string ItemDescription;
+	char Type;
+	int Value;
+	int ResaleValue;
+	int SaleValue;
+
+public:
+	
+	Item();
+
+	Item(std::string ItemName, std::string ItemDescription, char Type, int Value, int ResaleValue, int SaleValue);
+
+	~Item();
+
+	std::string GetItemWord(char ItemType) const;
+
+	void SetItemWord(char ChangeType, std::string Change);
+
+	int GetItemValue(char Type) const;
+
+	void SetItemValue(char Type, int Value);
+
+	char GetType() const;
+
+	void SetType(char Type);
+
+
 };
 
