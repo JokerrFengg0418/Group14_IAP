452f7ca9932aa2ae145f99ed2c0976308aaf789e
Themiei <themieigab@gmail.com>
2025-08-20 10:33:32 +0800

    Partial Commit, Not all Functions complete

--- Group14_IAP/Inventory.h
+++ Group14_IAP/Inventory.h
@@ -1,5 +1,34 @@
 #pragma once
 class Inventory
 {
+private:
+
+	Item* Inventory[25];
+	int Currency;
+	Item* ItemDatabase[50];
+	Item* EquippedItem;
+	Item* MonsterItemDatabase[50];
+	Item* WeaponDatabase[10];
+
+public:
+
+	void DrawInventory() const;
+
+	Item* GetInventory(std::string Name) const;
+
+	void SetInventory(std::string ItemName, int Number);
+
+	void RemoveItemFromInventory(std::string ItemName, int Number);
+
+	void DatabaseInitialisation();
+
+	int GetCurrency() const;
+
+	void SetCurrency(int NewAmount);
+
+	Item* DrawDatabase(char DatabaseType, std::string ItemName);
+
+	void UseItem(std::string ItemName);
+
 };
 
