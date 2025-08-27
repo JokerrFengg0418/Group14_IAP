a75d06072aeda45875c37c96451735ebf5ef4c54
Wei <kangweixuan69420@gmail.com>
2025-08-27 15:01:23 +0800

    pull

--- Group14_IAP/Logic.cpp
+++ Group14_IAP/Logic.cpp
@@ -1,6 +1,7 @@
 #include "Logic.h"
 #include "Combat.h"
 #include "Option.h"
+#include "Board.h"
 #include "Story.h"
 #include "Dungeon.h"
 
@@ -64,6 +65,7 @@ void Logic::TurnOrder() {
 		
 		story.ShowWave(0, 0);
 		GameOption.waitForEnter();
+		cutsceneCombatTutorial();
 		CombatHandler.startCombat('A');
 		CombatHandler.TurnOrder(GameOption.getPlayerInventory());
 		GlobalOrderSet(GlobalOrderGet() + 1);
@@ -131,5 +133,13 @@ void Logic::TurnOrder() {
 
 	}
 
+}
+
+void Logic::cutsceneCombatTutorial(Combat& combat)
+{
+	std::cout << "Welcome to the Combat Tutorial!";
+	std::cout << "Fighting is really easy! Just be within 2 tiles of the enemy";
+	std::cout << "In order for you to initiate the attack!";
+	std::cout << "You can also, cycle the attack if there are multiple enemies\n nearby you and you want to choose who to attack! you can do this with A and D keys";
 }
 
