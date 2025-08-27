6b28c8eb31a112c3ea4b27e23c615cf6980229b2
THAQIEF ANAQIE BIN OSMAN <252877A@mymail.nyp.edu.sg>
2025-08-27 23:08:19 +0800

    Fixes to logic so that choicId in story is used

--- Group14_IAP/Story.cpp
+++ Group14_IAP/Story.cpp
@@ -295,25 +295,25 @@ int Story::ShowWave(int wave, int choiceId)
                         }
                     }
                 }
-                if(Questcheck == false){
-                    if (entry.choicetext[pick - 1].find("Shop") != std::string::npos)
-                    {
-                        return 1; // shop triggered
-                    }
+                //if(Questcheck == false){
+                //    if (entry.choicetext[pick - 1].find("Shop") != std::string::npos)
+                //    {
+                //        return 1; // shop triggered
+                //    }
             
-                    // 👉 Detect if choice opens dungeon
-                    if (entry.choicetext[pick - 1].find("Dungeon") != std::string::npos)
-                    {
-                        return 2; // dungeon triggered
-                    }
-                }
+                //    // 👉 Detect if choice opens dungeon
+                //    if (entry.choicetext[pick - 1].find("Dungeon") != std::string::npos)
+                //    {
+                //        return 2; // dungeon triggered
+                //    }
+                //}
             
-                // Move to next choice if available
-                if (pick > 0 && pick <= entry.nextChoices.size())
-                {
-                    int nextChoiceId = entry.nextChoices[pick - 1];
-                    return ShowWave(wave, nextChoiceId);
-                }
+                //// Move to next choice if available
+                //if (pick > 0 && pick <= entry.nextChoices.size())
+                //{
+                //    int nextChoiceId = entry.nextChoices[pick - 1];
+                //    return ShowWave(wave, nextChoiceId);
+                //}
             
             }
         }
